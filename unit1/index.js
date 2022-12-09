4fe73f938cd8ac248ca87af514471898269b8ef3
bohdana <halenkobohdana@gmail.com>
2022-12-09 10:39:05 +0200

    Update index.js

--- unit1/index.js
+++ unit1/index.js
@@ -75,3 +75,27 @@
 // console.log(message.toLowerCase()); // "welcome to bahamas!"
 // console.log(message.toUpperCase()); // "WELCOME TO BAHAMAS!"
 
+
+
+
+
+//04.1
+
+    let name = "Иван";
+    let age = 25;
+    let company = "ITVDN";
+    let position = "Frontend Developer";
+
+        // инициализируйте переменную message
+        // значение должно быть создано с помощью шаблонной строки и с помощью конкатенации.
+        // строковое значение должно быть создано на основе значений переменных name, age, company, position
+        // Значение должно быть составлено по шаблону:
+        // Привет, меня зовут Иван. Мне 25 лет. Я работаю на должности Frontend Developer в компании ITVDN. 
+
+    // let message = "Привет, меня зовут " + name + ". Мне " + age + " лет. Я работаю на должности " + position + " в компании " + company + ".";
+    let message = `Привет, меня зовут ${name}. Мне ${age} лет. Я работаю на должности ${position} в компании ${company}.`
+    console.log(message);
+    
+//04.2
+
+//04.3
