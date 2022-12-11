93f978d0d34249557cd25ba00c37bf0576552ac8
bohdana <halenkobohdana@gmail.com>
2022-12-11 16:36:42 +0200

    add

--- unit1/index.js
+++ unit1/index.js
@@ -262,6 +262,28 @@ function getSubscriptionPrice(type) {
       price = 50; 
       break;
   }
-
   return price;
+}
+
+
+
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  let message;
+
+switch (password) {
+  case null:
+    message = "Canceled by user!";
+    break;
+
+  case ADMIN_PASSWORD:
+    message = "Welcome!";
+    break;
+
+  default:
+    message = "Access denied, wrong password!";
+}
+
+  return message;
 }
